0df1170e85d31894458dfbba33a141da547e116c
TJ Turnage <t.j.turnage@gmail.com>
2022-08-14 20:37:50 -0400

    - more steps and callbacks

--- fsw.py
+++ fsw.py
@@ -66,7 +66,7 @@ except:
 bold = {'font-weight': 'bold'}
 feedback = {'border': '2px gray solid', 'padding':'1em'}
 
-card_content = [
+top_content = [
             dbc.CardBody([html.H1("Forecast Search Wizard", className="card-title",style={'font-weight': 'bold', 'font-style': 'italic'}),
                 html.H4(
                     "An application to search National Weather Service Text Products by keywords",
@@ -85,7 +85,7 @@ card_content = [
 step_one = [
             dbc.CardBody([html.H5("Step 1", className="card-title",style=bold),
                 html.P(
-                    "Enter Products in upper case and separated by spaces. Click to create product list.",
+                    "Enter Words or phrases you want to search for. This needs to be in a list format.",
                     className="card-text",
                 ),
             ])
@@ -94,7 +94,7 @@ step_one = [
 step_two = [
             dbc.CardBody([html.H5("Step 2", className="card-title",style=bold),
                 html.P(
-                    "Choose Range of Years to Search",
+                    "Enter Products in upper case and separated by spaces. Click to create product list.",
                     className="card-text",
                 ),
             ])
@@ -102,6 +102,15 @@ step_two = [
 
 step_three = [
             dbc.CardBody([html.H5("Step 3", className="card-title",style=bold),
+                html.P(
+                    "Choose Range of Years to Search",
+                    className="card-text",
+                ),
+            ])
+]
+
+step_four = [
+            dbc.CardBody([html.H5("Step 4", className="card-title",style=bold),
                 html.P(
                     "Provide Details About How to Search",
                     className="card-text",
@@ -133,22 +142,31 @@ script_args = FSW()
 app.layout = dbc.Container(
     html.Div([
         dbc.Row(dbc.Col(html.Div(html.Hr()))),
-        dbc.Row(dbc.Card(card_content, color="secondary", inverse=True)),
+        dbc.Row(dbc.Card(top_content, color="secondary", inverse=True)),
         dbc.Row(dbc.Card(step_one, color="info", inverse=True), style={'padding':'1em'}),
         dbc.Row(
             html.Div([
                 dbc.InputGroup([
-                    dbc.Input(id='list-in',placeholder='Example ... AFDGRR AFDAPX', type='text'),
-                    dbc.Button("Create Product List",id='submit-button', n_clicks=0),
+                    dbc.Input(id='input_words_list',placeholder='Example ... ["SEA BREEZE", "SEABREEZE"]', type='text'),
+                    dbc.Button("Submit Input Words",id='input_words_list_submit', n_clicks=0),
             ], style={'padding':'1em'}),
-                html.Div(id='list-out', style=feedback,)])
+                html.Div(id='input_words_list-out', style=feedback,)])
         ),
         dbc.Row(dbc.Card(step_two, color="info", inverse=True), style={'padding':'1em'}),
+        dbc.Row(
+            html.Div([
+                dbc.InputGroup([
+                    dbc.Input(id='forecast_product_list',placeholder='Example ... AFDGRR AFDAPX', type='text'),
+                    dbc.Button("Create Product List",id='forecast_product_list_submit', n_clicks=0),
+            ], style={'padding':'1em'}),
+                html.Div(id='forecast_product_list-out', style=feedback,)])
+        ),
+        dbc.Row(dbc.Card(step_three, color="info", inverse=True), style={'padding':'1em'}),
         dbc.Row([
             html.Div([
                 dbc.Col(
                     dcc.RangeSlider(
-                    id="slider-input",
+                    id="slider_values",
                     min=1996,
                     max=this_year,
                     step=1,
@@ -156,7 +174,7 @@ app.layout = dbc.Container(
                     marks={i: str(i) for i in range(1996,next_year)},
                     ),style={'padding':'1.2em'},
             ),
-            html.Div(id='slider-values', style=feedback,)
+            html.Div(id='slider_values-out', style=feedback,)
 
     ])
     
@@ -164,7 +182,7 @@ app.layout = dbc.Container(
     style={"padding": "1em"},
     
     ),
-        dbc.Row(dbc.Card(step_three, color="info", inverse=True), style={'padding':'1em'}),
+        dbc.Row(dbc.Card(step_four, color="info", inverse=True), style={'padding':'1em'}),
         dbc.Row([
             dbc.Col(
                 html.Div([
@@ -209,22 +227,34 @@ app.layout = dbc.Container(
     ])
 )
 
+# input words
+@app.callback(Output("input_words_list-out", "children"),
+                [Input("input_words_list_submit","n_clicks")],
+                [State("input_words_list","value")])
+def create_list(n_clicks,myvalue):
+    input_string = str(myvalue)
+    script_args.input_word_list = input_string
+    return str(input_string)
 
-@app.callback(Output(component_id='list-out', component_property='children'),
-                [Input(component_id='submit-button',component_property='n_clicks')],
-                [State('list-in','value')])
+# product list
+@app.callback(Output("forecast_product_list-out", "children"),
+                [Input("forecast_product_list_submit","n_clicks")],
+                [State("forecast_product_list","value")])
 def create_list(n_clicks,myvalue):
     input_string = str(myvalue)
     product_list = input_string.split(' ')
+    script_args.input_word_list = product_list
     return str(product_list)
 
-@app.callback(Output(component_id='new-text', component_property='children'),
-                [Input(component_id='refresh-text',component_property='n_clicks')],)
-def get_text_output(n_clicks):
-    if n_clicks%2 == 0:
-        return text_data
-    else:
-        return ""
+# slider values
+@app.callback(Output("slider_values-out", "children"),
+                [Input('slider_values',"value")])
+def update_output(value):
+    start_year = value[0]
+    script_args.start_year = start_year
+    end_year = value[1]
+    script_args.end_year = end_year
+    return 'start_year = {} ........ end_year = {}'.format(start_year,end_year)
 
 @app.callback(
     Output("isAnd-out", "children"),
@@ -250,14 +280,13 @@ def on_form_change(isGrep_value):
     script_args.isGrep = isGrep_value
     return template
 
-@app.callback(Output(component_id='slider-values', component_property='children'),
-                [Input('slider-input',component_property='value')])
-def update_output(value):
-    start_year = value[0]
-    script_args.start_year = start_year
-    end_year = value[1]
-    script_args.end_year = end_year
-    return 'start_year = {} ........ end_year = {}'.format(start_year,end_year)
+@app.callback(Output("new-text", "children"),
+                [Input("refresh-text","n_clicks")],)
+def get_text_output(n_clicks):
+    if n_clicks%2 == 0:
+        return text_data
+    else:
+        return ""
 
 def make_dataframe(text):
     dts = []
@@ -269,19 +298,26 @@ def make_dataframe(text):
             dts.append(values[0])
             product.append(values[1][1:])
 
+#@app.callback(Output("slider_values-out", "children"),
+#                [Input('slider_values',"value")])
+#def display_arguments():
+#    template = "{} {} {} {}".format(script_args.script_args.isGrep,script_args.start_year)
+#    print(template)
+#    return
 
-    dts_pd = pd.to_datetime(dts,infer_datetime_format=True)
-    data = {'dts':dts_pd, 'product':product}
-    df_full = pd.DataFrame(data)
-    df_full.set_index('dts', inplace=True)
-    df = df_full[df_full['product'] == 'AFDGRR']
-    monthly = df.resample('M').count()
-    #print(monthly)
-    x=df.index
-    y=monthly['product']
-    fig = go.Figure(data=go.Scatter(x=x, y=y))
-    fig.show()
-    return
+
+#    dts_pd = pd.to_datetime(dts,infer_datetime_format=True)
+#    data = {'dts':dts_pd, 'product':product}
+#    df_full = pd.DataFrame(data)
+#    df_full.set_index('dts', inplace=True)
+#    df = df_full[df_full['product'] == 'AFDGRR']
+#    monthly = df.resample('M').count()
+#    #print(monthly)
+#    x=df.index
+#    y=monthly['product']
+#    fig = go.Figure(data=go.Scatter(x=x, y=y))
+#    fig.show()
+
 
 if __name__ == '__main__':
     app.run_server()

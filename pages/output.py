cbdc3d3130661d7c4df279236a38dcea199f1ce9
TJ Turnage <t.j.turnage@gmail.com>
2022-09-04 20:02:03 -0400

    - moved fsw.py

--- pages/output.py
+++ pages/output.py
@@ -2,7 +2,7 @@ import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-dash.register_page(__name__)
+dash.register_page(__name__, order=2, location = "sidebar")
 feedback = {'border': '2px gray solid', 'padding':'1em'}
 layout = dbc.Container([
 
